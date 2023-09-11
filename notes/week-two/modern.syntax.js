852a776adfec145288c8f1171eeb4293abf9cfd2
Bruce Christie <bqchristie@gmail.com>
2023-09-11 10:01:01 -0400

    notes and outline for week 2

---
+++ notes/week-two/modern.syntax.js
@@ -0,0 +1,51 @@
+// Arrow functions
+
+
+// Lexical This
+
+
+// 
+/**
+ * Destructing Object Paramaters
+ * 
+ * 1. destructure
+ * 2. rename
+ * 3. default values
+ */
+const robot1 = {
+    name: 'Robby',
+    color: 'Silver',
+    noise: 'Beep',
+}
+
+
+
+
+/**
+ * Arrays
+ * 
+ * 1. for loop
+ * 2. forEach
+ * 3. destructure
+ * 4. spread
+ * 5. map
+ * 6. reduce
+ */
+
+
+/**
+ * Strings
+ * 
+ * Template Literals 
+ *  
+ */
+
+
+/**
+ * Errors
+ * try/catch/finally
+ * throw Error
+ * 
+ * 
+ */
+
---
+++ notes/week-two/object.oriented.js
@@ -0,0 +1,13 @@
+//Object Literal Notiation - basically anything inside
+
+
+// The "class" keyword
+
+
+// private  methods and properties 
+
+
+// Getters and setters
+
+
+//Inherticance
