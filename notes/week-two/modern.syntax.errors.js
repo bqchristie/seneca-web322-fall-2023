cacded89e4e57c9373e7ef3eb58efc27366437cc
Bruce Christie <bqchristie@gmail.com>
2023-09-14 15:35:31 -0400

    updates for erors

--- notes/week-two/modern.syntax.errors.js
+++ notes/week-two/modern.syntax.errors.js
@@ -6,3 +6,21 @@
  * 3. throwing an error
  * 
  */
+
+
+console.log(isNaN(2))
+console.log(isNaN("sassa"))
+
+// lodash
+
+try {
+    throw new Error("This is our special error")
+
+} catch(err) {
+    console.error(err)
+    
+} finally {
+    console.log("finally")
+}
+
+console.log("Done")
