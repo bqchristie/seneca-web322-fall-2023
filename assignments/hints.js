eec07c3bf834ba79bf8ee331da48aa5d2fc20338
Bruce Christie <bqchristie@gmail.com>
2023-09-14 17:07:39 -0400

    updated hints

---
+++ assignments/hints.js
@@ -0,0 +1,33 @@
+// Event Emmitter
+const {EventEmitter} = require("events")
+
+const trafficLightsEventEmitter = new EventEmitter()
+
+// on / emit
+
+// Structuring data
+const lights = [
+    {color:"red", duration:5000},
+    {color:"yellow", duration:2000},
+    {color:"green", duration:5000},
+]
+
+
+let colorIdx = 0
+
+
+// Create a changle color methods
+function changeColor() {
+    const light = console.log(lights[0].color)
+    console.log(lights[colorIdx].color)
+    colorIdx++
+
+    if(colorIdx==3) {
+        colorIdx = 0;
+    }
+    changeColor()
+}
+
+
+// setInterval ot setTimeout
+changeColor()
